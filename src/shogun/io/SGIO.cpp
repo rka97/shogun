f0e76a1718b165298bd493feb02898267f41ca3b
Ahmed Essam <theartful.ae@gmail.com>
2019-07-22 01:57:47 +0200

    Use spdlog in SGIO
    We have two streams (stdout, stderr) that can be redirected to
    any number of different sinks

--- src/shogun/io/SGIO.cpp
+++ src/shogun/io/SGIO.cpp
@@ -7,13 +7,17 @@
  */
 
 #include <shogun/io/SGIO.h>
+#include <shogun/lib/RefCount.h>
+#include <shogun/lib/Time.h>
 #include <shogun/lib/common.h>
 #include <shogun/lib/memory.h>
-#include <shogun/lib/Time.h>
 #include <shogun/mathematics/Math.h>
-#include <shogun/lib/RefCount.h>
-#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/async_logger.h>
+#include <spdlog/sinks/base_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/null_sink.h>
+#include <spdlog/spdlog.h>
 
 #include <sstream>
 #include <stdarg.h>
@@ -24,31 +28,127 @@
 #else
 #include <unistd.h>
 #endif
+#include <mutex>
 #include <stdlib.h>
 
 #ifdef _WIN32
 #define R_OK 4
 #endif
 
+#define FBUFSIZE 4096
+
 using namespace shogun;
+
+class SGIO::RedirectSink : public spdlog::sinks::base_sink<std::mutex>
+{
+public:
+	RedirectSink()
+	{
+		stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		stderr_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
+	}
+
+	void redirect_stdout(std::shared_ptr<spdlog::sinks::sink> sink_)
+	{
+		set_sink_(stdout_sink, sink_);
+	}
+
+	void redirect_stderr(std::shared_ptr<spdlog::sinks::sink> sink_)
+	{
+		set_sink_(stderr_sink, sink_);
+	}
+
+protected:
+	void sink_it_(const spdlog::details::log_msg& msg) override
+	{
+		if (msg.level == spdlog::level::err)
+			stderr_sink->log(msg);
+		else
+			stdout_sink->log(msg);
+	}
+
+	void flush_() override
+	{
+		stdout_sink->flush();
+		if (stdout_sink != stderr_sink)
+			stderr_sink->flush();
+	}
+
+	void set_pattern_(const std::string& pattern) override
+	{
+		set_formatter_(
+		    spdlog::details::make_unique<spdlog::pattern_formatter>(pattern));
+	}
+
+	void
+	set_formatter_(std::unique_ptr<spdlog::formatter> sink_formatter) override
+	{
+		formatter_ = std::move(sink_formatter);
+		stdout_sink->set_formatter(formatter_->clone());
+		if (stdout_sink != stderr_sink)
+			stderr_sink->set_formatter(formatter_->clone());
+	}
+
+private:
+	void set_sink_(
+	    std::shared_ptr<spdlog::sinks::sink>& old_sink,
+	    const std::shared_ptr<spdlog::sinks::sink>& new_sink)
+	{
+		flush();
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (new_sink)
+			old_sink = new_sink;
+		else
+			old_sink = std::make_shared<spdlog::sinks::null_sink_mt>();
+	}
+
+	std::shared_ptr<spdlog::sinks::sink> stdout_sink;
+	std::shared_ptr<spdlog::sinks::sink> stderr_sink;
+};
 
 SGIO::SGIO()
     : show_progress(false), location_info(MSG_NONE),
       syntax_highlight(true)
 {
 	m_refcount = new RefCount();
-	logger = spdlog::stdout_color_mt("console");
+	io_sink = std::make_shared<RedirectSink>();
+
+	const int queue_size = 128;
+	const int n_threads = 1;
+	spdlog::init_thread_pool(queue_size, n_threads);
+
+	io_logger = std::make_shared<spdlog::async_logger>(
+	    "global", io_sink, spdlog::thread_pool(),
+	    spdlog::async_overflow_policy::block);
 	update_pattern();
 }
 
 SGIO::SGIO(const SGIO& orig)
     : show_progress(orig.get_show_progress()),
       location_info(orig.get_location_info()),
-      syntax_highlight(orig.get_syntax_highlight()),
-	  logger(orig.logger)
+      syntax_highlight(orig.get_syntax_highlight()), io_sink(orig.io_sink),
+      io_logger(orig.io_logger)
 {
 	m_refcount = new RefCount();
-	update_pattern();
+}
+
+std::string SGIO::format(const char* fmt, ...) const
+{
+	char str[FBUFSIZE];
+
+	va_list list;
+	va_start(list, fmt);
+	vsnprintf(str, sizeof(str), fmt, list);
+	va_end(list);
+
+	return std::string(str);
+}
+
+void SGIO::print(
+    EMessageType prio, const spdlog::source_loc& loc,
+    const std::string& msg) const
+{
+	io_logger->log(loc, static_cast<spdlog::level::level_enum>(prio), msg);
 }
 
 void SGIO::done()
@@ -89,12 +189,12 @@ char* SGIO::skip_blanks(char* str)
 
 EMessageType SGIO::get_loglevel() const
 {
-	return static_cast<EMessageType>(logger->level());
+	return static_cast<EMessageType>(io_logger->level());
 }
 
 void SGIO::set_loglevel(EMessageType level)
 {
-	logger->set_level(static_cast<spdlog::level::level_enum>(level));
+	io_logger->set_level(static_cast<spdlog::level::level_enum>(level));
 }
 
 char* SGIO::c_string_of_substring(substring s)
@@ -198,5 +298,19 @@ void SGIO::update_pattern()
 	else
 		pattern_builder << "%l] ";
 
-	logger->set_pattern(pattern_builder.str());
+	pattern_builder << "%v";
+
+	io_logger->set_pattern(pattern_builder.str());
+}
+
+void SGIO::redirect_stdout(std::shared_ptr<spdlog::sinks::sink> sink)
+{
+	io_sink->redirect_stdout(sink);
+	update_pattern();
+}
+
+void SGIO::redirect_stderr(std::shared_ptr<spdlog::sinks::sink> sink)
+{
+	io_sink->redirect_stderr(sink);
+	update_pattern();
 }
